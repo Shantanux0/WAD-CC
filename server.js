7f88c71c8e22deef5825c01296b9337c91c2c097
Shanatanu Kale <kaleshantanu2260@gmail.com>
2025-05-06 02:49:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
+// Create HTTP server using Node JS
+
+
+
+
+// Import the built-in http module
+const http = require('http');
+
+// Define the server port
+const PORT = 3000;
+
+// Create the server
+const server = http.createServer((req, res) => {
+  // Set the response header
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Send the response body
+  res.end('Hello from Node.js HTTP server!');
+});
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+
+
+//TO Run The code 
+//node server.js
 
